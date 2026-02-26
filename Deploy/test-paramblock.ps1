f0212010842e02f04611aec2ae5a119b4eeae4df
NikAutomates <137150756+NikAutomates@users.noreply.github.com>
2026-02-26 14:07:36 -0500

    Update test-paramblock.ps1

--- Deploy/test-paramblock.ps1
+++ Deploy/test-paramblock.ps1
@@ -3,12 +3,14 @@ param(
     [string]$azure_token
 )
 
-
 # GRAPH
-Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups" -Headers @{ Authorization = "Bearer $graph_token" } | select-object value
+Invoke-RestMethod `
+  -Uri "https://graph.microsoft.com/v1.0/groups" `
+  -Headers @{ Authorization = "Bearer $graph_token" } |
+  Select-Object value
 
+# ARM
 Invoke-RestMethod `
   -Uri "https://management.azure.com/subscriptions?api-version=2020-01-01" `
-  -Headers @{ Authorization = "Bearer $azure_token" } `
-  -SkipHttpErrorCheck `
-  -Verbose
+  -Headers @{ Authorization = "Bearer $azure_token" } |
+  Select-Object value
