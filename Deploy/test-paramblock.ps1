9cc36e485868e1be824849645c6f9e30db73e6be
NikAutomates <137150756+NikAutomates@users.noreply.github.com>
2026-02-26 14:15:42 -0500

    Remove visible output for Graph API call
    Removed unnecessary output for Microsoft Graph API call.

--- Deploy/test-paramblock.ps1
+++ Deploy/test-paramblock.ps1
@@ -3,14 +3,6 @@ param(
     [string]$azure_token
 )
 
-# GRAPH – force visible output
-Invoke-RestMethod `
-  -Uri "https://graph.microsoft.com/v1.0/groups" `
-  -Headers @{ Authorization = "Bearer $graph_token" } |
-  ConvertTo-Json -Depth 10 |
-  Write-Host
-
-
 # ARM – force visible output
 Invoke-RestMethod `
   -Uri "https://management.azure.com/subscriptions?api-version=2020-01-01" `
