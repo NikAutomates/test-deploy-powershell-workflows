ccc34b2e77eec26f7f5f1e9ef4b2436285a0b1ac
NikAutomates <137150756+NikAutomates@users.noreply.github.com>
2026-02-26 14:09:20 -0500

    Add comments for GRAPH and ARM sections

--- Deploy/test-paramblock.ps1
+++ Deploy/test-paramblock.ps1
@@ -3,13 +3,13 @@ param(
     [string]$azure_token
 )
 
-# GRAPH
+# GRAPH (table)
 Invoke-RestMethod `
   -Uri "https://graph.microsoft.com/v1.0/groups" `
   -Headers @{ Authorization = "Bearer $graph_token" } |
   Select-Object value
 
-# ARM
+# ARM (table)
 Invoke-RestMethod `
   -Uri "https://management.azure.com/subscriptions?api-version=2020-01-01" `
   -Headers @{ Authorization = "Bearer $azure_token" } |
