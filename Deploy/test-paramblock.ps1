6dd9bd4354084e40d9e3cee101d6d58fbb5a1a97
NikAutomates <137150756+NikAutomates@users.noreply.github.com>
2026-02-26 14:04:55 -0500

    Update REST API calls in test-paramblock.ps1

--- Deploy/test-paramblock.ps1
+++ Deploy/test-paramblock.ps1
@@ -5,8 +5,10 @@ param(
 
 
 # GRAPH
-Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups" -Headers @{ Authorization = "Bearer $graph_token" }
+#Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups" -Headers @{ Authorization = "Bearer $graph_token" }
 
-
-# ARM
-Invoke-RestMethod -Uri "https://management.azure.com/subscriptions/?api-version=2022-12-01" -Headers @{ Authorization = "Bearer $azure_token" }
+Invoke-RestMethod `
+  -Uri "https://management.azure.com/subscriptions?api-version=2020-01-01" `
+  -Headers @{ Authorization = "Bearer $azure_token" } `
+  -SkipHttpErrorCheck `
+  -Verbose
