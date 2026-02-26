b1b6cffac6b4c74c762ff6b61276162584477a14
NikAutomates <137150756+NikAutomates@users.noreply.github.com>
2026-02-26 13:55:32 -0500

    Implement API calls for Graph and Azure Management
    Added REST API calls to Microsoft Graph and Azure Management.

--- Deploy/test-paramblock.ps1
+++ Deploy/test-paramblock.ps1
@@ -3,5 +3,9 @@ param(
     [string]$azure_token
 )
 
-write-host $graph_token
-write-host $azure_token
+
+# GRAPH
+Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/organization" -Headers @{ Authorization = "Bearer $graph_token" }
+
+# ARM
+Invoke-RestMethod -Uri "https://management.azure.com/subscriptions?api-version=2020-01-01" -Headers @{ Authorization = "Bearer $azure_token" }
