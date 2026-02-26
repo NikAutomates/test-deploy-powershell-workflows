a94e8e5613f5fed654eed992d5ad14f5ca5769ee
NikAutomates <137150756+NikAutomates@users.noreply.github.com>
2026-02-26 14:01:04 -0500

    Add Azure Management API call to PowerShell script

--- Deploy/test-paramblock.ps1
+++ Deploy/test-paramblock.ps1
@@ -7,5 +7,6 @@ param(
 # GRAPH
 Invoke-RestMethod -Uri "https://graph.microsoft.com/v1.0/groups" -Headers @{ Authorization = "Bearer $graph_token" }
 
+
 # ARM
 Invoke-RestMethod -Uri "https://management.azure.com/subscriptions?api-version=2020-01-01" -Headers @{ Authorization = "Bearer $azure_token" }
